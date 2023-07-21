6c681a9d38c8735b092d60f40b084b657113c2ce
Your Name <you@example.com>
2023-07-21 11:40:27 +0530

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<object> internalData;
+
+        public LargeDataCollection(IEnumerable<object> initialData)
+        {
+            internalData = new List<object>(initialData);
+        }
+        public void Add(object item)
+        {
+            internalData.Add(item);
+        }
+        public void Remove(object item)
+        {
+            internalData.Remove(item);
+        }
+
+        public object Get(int index)
+        {
+            return internalData[index];
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+                internalData.Clear();
+                internalData = null;
+            }
+
+        }
+    }
+    internal class Class1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+
+            using (LargeDataCollection collection = new LargeDataCollection(new List<object> { 1, "Hello", DateTime.Now }))
+            {
+                collection.Add(42);
+                collection.Add("World");
+                collection.Remove("Hello");
+
+
+                Console.WriteLine(collection.Get(0));
+                Console.WriteLine(collection.Get(1));
+                Console.WriteLine(collection.Get(2));
+                Console.WriteLine(collection.Get(3));
+                Console.Read();
+            }
+        }
+    }
+
+}
